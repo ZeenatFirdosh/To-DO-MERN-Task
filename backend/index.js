ad7828f799f39594297e5ca73f7555f14db4a06f
Zeenat <zeenatzfq052@gmail.com>
2024-05-25 12:54:26 +0530

    get

--- backend/index.js
+++ backend/index.js
@@ -14,5 +14,9 @@ app.use(express.json());
 
 app.use('/api', taskRoutes);
 
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
