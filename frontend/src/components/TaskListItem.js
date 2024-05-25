6daeedad50fee3cfc5f2bbb15005e61c4e85bd1a
Zeenat <zeenatzfq052@gmail.com>
2024-05-25 13:10:12 +0530

    changes

--- frontend/src/components/TaskListItem.js
+++ frontend/src/components/TaskListItem.js
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 import { deleteTask } from '../api';
 
 const TaskListItem = ({ task, onDelete }) => {
+console.log(task)
   return (
     <div className="border rounded-md p-4 mb-4 flex justify-between items-center">
       <div>
+        <div className='flex items-center gap-4 justify-between'>
         <h3 className="text-lg font-bold">{task.title}</h3>
-        <p className="text-sm">{task.status}</p>
+        <span className="text-sm">{task.status}</span>
+        </div>
+        <p className="text-sm">{task.description}</p>
         <p className="text-sm">{task.dueDate.slice(0,10)}</p>
       </div>
       <div className="flex space-x-2">
